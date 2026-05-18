0695f4b6f25d8b77932aa8981053acf17097bb48
Andrey Polyakov <andrey@polyakov.im>
2026-05-18 15:42:08 +0300

    feat: добавлены фабрики клиентов для работы с Dadata

---
+++ src/ClearClientFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Glook\Dadata;
+
+use Glook\Dadata\Generated\Clear\Authentication\DadataAuthorizationAuthentication;
+use Glook\Dadata\Generated\Clear\Authentication\DadataSecretAuthentication;
+use Glook\Dadata\Generated\Clear\Client;
+use Http\Client\Common\Plugin;
+use Jane\OpenApiRuntime\Client\Plugin\AuthenticationRegistry;
+
+final class ClearClientFactory
+{
+    /**
+     * @param Plugin[] $additionalPlugins
+     */
+    public static function create(string $token, string $secret, array $additionalPlugins = []): Client
+    {
+        return Client::create(null, array_merge([
+            new AuthenticationRegistry([
+                new DadataAuthorizationAuthentication('Token '.$token),
+                new DadataSecretAuthentication($secret),
+            ]),
+        ], $additionalPlugins));
+    }
+}
---
+++ src/SuggestionsClientFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Glook\Dadata;
+
+use Glook\Dadata\Generated\Suggestions\Authentication\DadataAuthorizationAuthentication;
+use Glook\Dadata\Generated\Suggestions\Authentication\DadataSecretAuthentication;
+use Glook\Dadata\Generated\Suggestions\Client;
+use Http\Client\Common\Plugin;
+use Jane\OpenApiRuntime\Client\Plugin\AuthenticationRegistry;
+
+final class SuggestionsClientFactory
+{
+    /**
+     * @param Plugin[] $additionalPlugins
+     */
+    public static function create(string $token, string $secret, array $additionalPlugins = []): Client
+    {
+        return Client::create(null, array_merge([
+            new AuthenticationRegistry([
+                new DadataAuthorizationAuthentication('Token '.$token),
+                new DadataSecretAuthentication($secret),
+            ]),
+        ], $additionalPlugins));
+    }
+}
